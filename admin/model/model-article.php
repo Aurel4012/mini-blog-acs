94bae16ced44cdd6952e40a12005dd9bcebde24a
AURELIEN LECOQ <aurelien.lecoq@free.fr>
2017-11-20 14:31:44 +0100

    modif article

--- admin/model/model-article.php
+++ admin/model/model-article.php
@@ -1,11 +1,11 @@
 <?php
 if (isset($_POST['ID_ARTICLE'])){
 $ID_ARTICLE = htmlspecialchars(($_POST['ID_ARTICLE']));
-header('Location: ../model/model-article.php?ID_ARTICLE='.$ID_ARTICLE);
+// header('Location: ../model/model-article.php?ID_ARTICLE='.$ID_ARTICLE);
 
 }
 else{
-	header('Location: ../model/model-article.php');
+	// header('Location: ../model/model-article.php');
 }
 
 ?>
