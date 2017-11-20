1d3228ebe144a072eef797610d5012305745c3bc
Benouk <32901416+BNK313@users.noreply.github.com>
2017-11-20 15:57:08 +0100

    base ok
    ok

--- public/view/main.inc.php
+++ public/view/main.inc.php
@@ -1,23 +1,15 @@
 <main>
-          <!-- Page Content -->
     <div class="container">
-
       <div class="row">
-
-        <!-- Post Content Column -->
         <div class="col-lg-8">
-
-          <!-- Date/Time -->
-          <p>Posted on January 1, 2017 at 12:00 PM</p>
+          <p>Posted on January 1, 2017 at 12:00 PM</p> <!-- généré en php -->
 
           <hr>
 
-          <!-- Preview Image -->
           <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
 
           <hr>
 
-          <!-- Post Content -->
           <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
 
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
@@ -28,48 +20,19 @@
 
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
 
-          <hr>
-
-            </div>
-          
-
-        
-
-        <!-- Sidebar Widgets Column -->
+        </div>
         <div class="col-md-4 float-right">
-
-          <!-- Categories Widget -->
           <div class="card my-4 sticky-top mt-5">
             <h5 class="card-header">Categories</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Web Design</a>
-                    </li>
-                    <li>
-                      <a href="#">HTML</a>
-                    </li>
-                    <li>
-                      <a href="#">Freebies</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">JavaScript</a>
-                    </li>
-                    <li>
-                      <a href="#">CSS</a>
-                    </li>
-                    <li>
-                      <a href="#">Tutorials</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <div class="card-body">
+                <select class="col-11">
+                      <option>7% (Tranquille!)</option>
+                      <option>15% (Ca va je gère!)</option>
+                      <option>20% (C'est le dernier!)</option>
+                      <option>70% (Il est des nôtres!!)</option>
+                      <option> +70% (J'aoi% perdu mùon Mossepate!)</option>
+                    </select>
+
             </div>
           </div>
 
