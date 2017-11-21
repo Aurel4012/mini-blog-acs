d6cd71cecb9ab6d669d635681faa77b8103d162a
Benouk <32901416+BNK313@users.noreply.github.com>
2017-11-21 11:09:10 +0100

    footer ok

--- public/view/footer.inc.php
+++ public/view/footer.inc.php
@@ -1,4 +1,4 @@
-		<div id="footer" class="text-center col-12 bg-secondary sticky-bottom">
+		<div id="footer" class="text-center col-12 bg-secondary fixed-bottom mt-5">
 			<p> ©2017 Copyright</p> 
 		</div>
 	</body>
