08687c1f01c3fb29d982e33e63aeb9c9126b033c
Benouk <32901416+BNK313@users.noreply.github.com>
2017-11-21 11:03:55 +0100

    stickybottomfooter

--- public/view/footer.inc.php
+++ public/view/footer.inc.php
@@ -1,4 +1,4 @@
-		<div id="footer" class="text-center col-12 bg-secondary">
+		<div id="footer" class="text-center col-12 bg-secondary sticky-bottom">
 			<p> ©2017 Copyright</p> 
 		</div>
 	</body>
