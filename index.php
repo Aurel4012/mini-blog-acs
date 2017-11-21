4cccec3759ca6f5bb0a6b89fd98b614825b9be9d
Benouk <32901416+BNK313@users.noreply.github.com>
2017-11-21 15:55:18 +0100

    maj lien index public php

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
     require_once ('public/controler/control-category.php');
     require_once ('public/controler/control-art.php');
     require_once ('../mini-blog-acs/admin/controler/control-admin-user.php');
+
+    require_once ('public\view\header.inc.php');
+    require_once ('public\view\main.inc.php');
+    require_once ('public\view\footer.inc.php');
 ?>
