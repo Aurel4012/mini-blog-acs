396270efe90ccac8c478965973b68ce988ef086c
AURELIEN LECOQ <aurelien.lecoq@free.fr>
2017-11-22 15:55:51 +0100

    test

--- admin/view/edit-article.php
+++ admin/view/edit-article.php
@@ -1,14 +1,14 @@
 <?php
-  // echo getcwd().'<br>'; 
+// echo getcwd().'<br>'; 
 	require_once('view/header.inc.php');
 ?>
 
 	
 <div class="container">
   <form method="post">
-
-    <label for="mini_img_import">Titre de l'article</label>
+  	 <label for="mini_img_import">Titre de l'article</label>
     <input type="text" name="titre_article" value="<?php echo $article_title ?>" class="form-control-file" id="titre_article">
+<<<<<<< HEAD
 
      	 <label for="mini_img_import">Image Actuelle	 <img src="<?php echo $article_preview_image ?>"></label>
   	 <label for="mini_img_import">Importer l'image miniature</label>
@@ -21,6 +21,11 @@
     <button href="./admin/controler/control-delete.php??id_article=3&bdd=$bdd" class="btn btn-danger lien-suppr"  method="post" name="id_article" value="<?php echo $id_article;?>">Supprimer</button></a>
     </form>
 =======
+  	 <label for="mini_img_import">Importer l'image miniature</label>
+    <input type="file" name="mini_img" class="form-control-file" id="mini_img" value="<?php echo $article_preview_image ?>">
+    <textarea id="mytextarea"><?php echo $article_text ?></textarea>
+    <button type="submit" class="btn btn-warning">Modifié</button>
+  </form>
 >>>>>>> 2a7a97f70058dadba046b31245339672472776d7
 <script type="text/javascript" src="../public/view/vendor/jquery/jquery.min.js"></script>
 <script type="text/javascript" src="../public/view/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -29,4 +34,4 @@
 <script type="text/javascript" src="../public/view/vendor/tinymce/edit.js"></script>
 </div>
 
-<!-- <?php require_once('../public/view/footer.inc.php');?> Voir si éventuellement nous gardons le footer ou non? --><?php require_once('../public/view/footer.inc.php');?>
+<?php require_once('../public/view/footer.inc.php');?>
