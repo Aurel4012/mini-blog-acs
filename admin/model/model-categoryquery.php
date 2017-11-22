553dec39db3af3aa7f7489d526c221eff299a0fa
CLucie <lucie.combrisson@laposte.net>
2017-11-22 11:15:33 +0100

    MAJ catégories

--- admin/model/model-categoryquery.php
+++ admin/model/model-categoryquery.php
@@ -6,25 +6,36 @@
 			$this->_bdd = $bdd;
 		}
 
-		public function getCategory($id_categorie){
-			$req = $this->prepare('SELECT * FROM table_categorie WHERE id_categorie = :id_categorie');
-			$req->execute(array(
-				'id_categorie' => $id_categorie));
-			$donnees = $req->fetch();
-			$id_categorie = new Categorie($sonnees);
-			return $id_categorie;
+		public function getList(){
+			$tab = [];
+			$reponse=$this->_bdd->prepare('SELECT * FROM table_categorie');
+			$reponse->execute();
+			while($donnees = $reponse->fetch()){
+				$tab[] =$donnees;
+			}
+			$reponse->closeCursor();
+			return $tab;
 		}
-		public function getBdd()
-	    {
-	        return $this->_bdd;
-	    }
+
+		// public function getCategory($id_categorie){
+		// 	$req = $this->prepare('SELECT * FROM table_categorie WHERE id_categorie = :id_categorie');
+		// 	$req->execute(array(
+		// 		'id_categorie' => $id_categorie));
+		// 	$donnees = $req->fetch();
+		// 	$id_categorie = new Categorie($sonnees);
+		// 	return $id_categorie;
+		// }
+		// public function getBdd()
+	 //    {
+	 //        return $this->_bdd;
+	 //    }
 
 	    
-	    public function setBdd($_bdd)
-	    {
-	        $this->_bdd = $_bdd;
+	 //    public function setBdd($_bdd)
+	 //    {
+	 //        $this->_bdd = $_bdd;
 
-	        return $this;
-	    }
+	 //        return $this;
+	 //    }
 	}
 ?>
