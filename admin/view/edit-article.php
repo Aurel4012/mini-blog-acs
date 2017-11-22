1986eecad666d5c721bbf8653742d73a7c976295
AURELIEN LECOQ <aurelien.lecoq@free.fr>
2017-11-22 11:37:21 +0100

    creation header footer admin

--- admin/view/edit-article.php
+++ admin/view/edit-article.php
@@ -1,6 +1,6 @@
 <?php
- // echo getcwd().'<br>'; 
-	require_once('../public/view/header.inc.php');
+// echo getcwd().'<br>'; 
+	require_once('view/header.inc.php');
 ?>
 
 	
@@ -11,6 +11,7 @@
   	 <label for="mini_img_import">Importer l'image miniature</label>
     <input type="file" name="mini_img" class="form-control-file" id="mini_img" value="<?php echo $article_preview_image ?>">
     <textarea id="mytextarea"><?php echo $article_text ?></textarea>
+    <button type="submit" class="btn btn-warning">Modifié</button>
   </form>
 <script type="text/javascript" src="../public/view/vendor/jquery/jquery.min.js"></script>
 <script type="text/javascript" src="../public/view/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -18,4 +19,5 @@
 <script type="text/javascript" src="../public/view/vendor/tinymce/tinymce.min.js"></script>
 <script type="text/javascript" src="../public/view/vendor/tinymce/edit.js"></script>
 </div>
+
 <?php require_once('../public/view/footer.inc.php');?>
